994184dc290a52577de890b80f313c30d32e8f84
hugopascual <hugopascual998@gmail.com>
2024-04-18 18:04:12 +0200

    capitals and more extra analysis

--- src/graphics/visualization.py
+++ src/graphics/visualization.py
@@ -6,6 +6,7 @@ import pandas as pd
 import plotly.graph_objects as go
 import requests
 import json
+from ast import literal_eval
 from shapely import (
     Point,
     from_geojson
@@ -19,8 +20,9 @@ from src.utils.common_functions import (
 from src.utils.constants import (
     EEE_MESH_3_FILEPATH,
     PARTIAL_RESULTS_DIR,
+    IP_URL,
     RESULTS_MODES,
-    IP_URL
+    REPLICATION_PACKAGE_DIR
 )
 
 MESH_FILEPATH = EEE_MESH_3_FILEPATH
@@ -143,8 +145,8 @@ def visualize_hunter_routes_results(filepath: str):
         (routes_results_df["outside_EEE"] == True)
     ]
 
-    origins_latitudes = routes_results_df["origin_latitude"].to_list()
-    origins_longitudes = routes_results_df["origin_longitude"].to_list()
+    origins_latitudes = routes_results_df["capital_origin_latitude"].to_list()
+    origins_longitudes = routes_results_df["capital_origin_longitude"].to_list()
     results_latitudes = routes_results_df["result_latitude"].to_list()
     results_longitudes = routes_results_df["result_longitude"].to_list()
 
@@ -205,10 +207,10 @@ def visualize_hunter_routes_results(filepath: str):
 
 def get_ip_location_via_cache(ip_address: str):
     url = f"{IP_URL}/{ip_address}"
-    details = json.loads(requests.get(
-        url=url).json()["details"])
+    details = json.loads(requests.get(url=url).json()["details"])
     anycast = False
     if "bogon" in details.keys():
+        print("IS BOGON")
         return 0, 0, False
 
     if "anycast" in details.keys():
@@ -217,8 +219,40 @@ def get_ip_location_via_cache(ip_address: str):
     return details["longitude"], details["latitude"], anycast
 
 
-def visualize_complete_route():
-    pass
+def visualize_complete_route(route_locations: list):
+    fig = go.Figure()
+    longitudes = [
+        coords[0]
+        for coords in route_locations
+    ]
+    latitudes = [
+        coords[1]
+        for coords in route_locations
+    ]
+
+    ip_address_list = [
+        coords[2]
+        for coords in route_locations
+    ]
+
+    fig.add_trace(
+        go.Scattergeo(
+            lon=longitudes,
+            lat=latitudes,
+            mode="markers+lines",
+            marker={
+                "color": "black",
+                "symbol": "circle"
+            },
+            hovertext=ip_address_list,
+            name="routes",
+            showlegend=False
+        )
+    )
+
+    # Layout
+    update_geo_layout(fig)
+    fig.show()
 
 
 # Show one country results
@@ -226,13 +260,35 @@ def visualize_complete_route():
 # ['AT', 'BE', 'BG', 'CY', 'CZ', 'DE', 'DK', 'EE', 'ES', 'FI', 'FR', 'GR',
 # 'HR', 'HU', 'IE', 'IS', 'IT', 'LT', 'LU', 'LV', 'MT', 'NL', 'NO', 'PL',
 # 'PT', 'RO', 'SE', 'SI', 'SK', 'LI']
-# ANALYSIS_MODE = RESULTS_MODES[1]
-# origin_country = "DK"
-# visualize_hunter_routes_results(
-#     f"{PARTIAL_RESULTS_DIR}"
-#     f"/routes_results_{ANALYSIS_MODE}_{origin_country}.csv")
+ANALYSIS_MODE = RESULTS_MODES[1]
+origin_country = "DK"
+#visualize_hunter_routes_results(
+#    f"{PARTIAL_RESULTS_DIR}"
+#    f"/routes_results_{ANALYSIS_MODE}_{origin_country}.csv")
+visualize_hunter_routes_results(
+    f"{REPLICATION_PACKAGE_DIR}/analysis_{ANALYSIS_MODE}_repetitions_1/"
+    f"routes_results_{ANALYSIS_MODE}.csv")
+
 
 # Show suspicious routes
-suspicious_routes_df = pd.read_csv(
-    f"{PARTIAL_RESULTS_DIR}/voting/suspicious_results_voting.csv")
-print(get_ip_location_via_cache("34.111.75.154"))
+#suspicious_routes_df = pd.read_csv(
+#    f"{PARTIAL_RESULTS_DIR}/voting/suspicious_results_voting.csv")
+#suspicious_routes_df = suspicious_routes_df.loc[
+#    (suspicious_routes_df["origin_country"] == "ES")
+#]
+#for index, row in suspicious_routes_df.iterrows():
+#    route = literal_eval(row["route"])
+#    locations = [(row["origin_longitude"], row["origin_latitude"], "origin")]
+#    for hop in route[1:]:
+#        if len(hop) == 0:
+#            continue
+#
+#        longitude, latitude, is_anycast = get_ip_location_via_cache(hop[0])
+#        if is_anycast or (latitude == 0 and longitude == 0):
+#            pass
+#        else:
+#            locations.append(
+#                (longitude, latitude, hop[0]))
+#    print(locations)
+#
+#    visualize_complete_route(locations)
