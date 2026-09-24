e98222b2ad3be8f5874078b08689513e29b1bce4
hugopascual <hugo.pascual.adan@upm.es>
2026-09-24 17:38:23 +0200

    feat: new statistics added

--- thesis_update.py
+++ thesis_update.py
@@ -1,10 +1,10 @@
 # external imports
 
 # internal imports
-from src.graphics.visualization import (
-    visualize_probes_selection_grid
-)
+from src.graphics.visualization import visualize_probes_selection_grid
 
+
+########################################################################################################################
 
 if __name__ == "__main__":
     visualize_probes_selection_grid(
